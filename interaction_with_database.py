36d24736793a68ec3bffa52de7b1b1b4c1ef6b8e
novd7 <novd7@yandex.ru>
2023-03-30 17:11:07 +0300

    added function of memorizing user's city coordinates  in sqlite database

---
+++ interaction_with_database.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+
+def create_bd():
+    """Function to create sqlite database to keep results of all users"""
+    try:
+        open("data.sqlite")
+    except FileNotFoundError:
+        con = sqlite3.connect("data.sqlite")
+        cur = con.cursor()
+        cur.execute("""
+            CREATE TABLE locality (
+                user_id INTEGER PRIMARY KEY,
+                latitude FLOAT NOT NULL,
+                longitude FLOAT NOT NULL
+            )
+        """)
+        con.commit()
+
+
+def insert_lat_lon_in_database(user_id: int, latitude: float, longitude: float):
+    """Function to add a new recording to database"""
+    create_bd()
+    con = sqlite3.connect("data.sqlite")
+    cur = con.cursor()
+    try:
+        cur.execute(f"""
+                    INSERT INTO locality (user_id, latitude, longitude)
+                    VALUES ({user_id}, {latitude}, {longitude})
+                """)
+    except sqlite3.IntegrityError:
+        cur.execute(f"""
+                        UPDATE locality SET
+                        latitude = {latitude}, longitude = {longitude}
+                        WHERE user_id = {user_id}
+                        """)
+    con.commit()
