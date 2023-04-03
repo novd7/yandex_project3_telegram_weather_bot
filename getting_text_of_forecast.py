c070894fc1cf7f99badba6d3d7dcb304ff72edb5
Никита <manticore1305@gmail.com>
2023-04-03 18:10:06 +0300

    small design changes of messages

--- getting_text_of_forecast.py
+++ getting_text_of_forecast.py
@@ -66,11 +66,11 @@ def translate_wind_dir(wind_dir: str) -> str:
 
 def get_message_text_for_day_forecast(data: dict):
     # print("data=", data)
-    message = f"Погода на {data['date']} в {data['place']}:\n\n"
-    for rus, eng, for_symbol in zip(("<b><u>Ночью</u></b> (0 - 5 часов)",
-                                     "<b><u>Утром</u></b> (6 - 11 часов)",
-                                     "<b><u>Днём</u></b> (12 - 17 часов)",
-                                     "<b><u>Вечером</u></b> (18 - 21 часов)"),
+    message = f"<b>❖ Погода на {data['date']} в городе {data['place']} ❖</b>\n\n"
+    for rus, eng, for_symbol in zip(("<b>•<u> Ночью</u></b> (0 - 5 часов)",
+                                     "<b>•<u> Утром</u></b> (6 - 11 часов)",
+                                     "<b>•<u> Днём</u></b> (12 - 17 часов)",
+                                     "<b>•<u> Вечером</u></b> (18 - 23 часов)"),
                                     ("night", "morning", "day", "evening"),
                                     ("night", "day", "day", "night")):
         # print("eng=", eng)
@@ -78,26 +78,26 @@ def get_message_text_for_day_forecast(data: dict):
                    f"   {get_weather_condition_symbol(data['weather'][eng]['condition'], for_symbol)} " \
                    f"{translate_condition(data['weather'][eng]['condition'])} " \
                    f"<u><b>{data['weather'][eng]['temperature']}°C</b></u>\n"
-        message += f"    Ветер: {translate_wind_dir(data['weather'][eng]['wind_dir'].upper())}, " \
+        message += f"  ‣ Ветер: {translate_wind_dir(data['weather'][eng]['wind_dir'].upper())}, " \
                    f"{data['weather'][eng]['wind_speed']} м/с\n"
-        message += f"    Влажность: {data['weather'][eng]['humidity']}%\n"
-        message += f"    Давление: {data['weather'][eng]['pressure']} мм рт. ст.\n"
+        message += f"  ‣ Влажность: {data['weather'][eng]['humidity']}%\n"
+        message += f"  ‣ Давление: {data['weather'][eng]['pressure']} мм рт. ст.\n\n"
     # print("message=", message)
     return message
 
 
 def get_message_text_for_week_forecast(data: dict):
-    message = f"Прогноз на ближайшие 7 дней:\n\n"
+    message = f"<b>❖ Прогноз на ближайшие 7 дней ❖</b>\n\n"
     for date in data.keys():
         message += f"<b><u>{date}</u></b>\n"
-        message += f"    Ночью (0 - 5 часов): \n" \
+        message += f"  • Ночью (0 - 5 часов): \n" \
                    f"        {get_weather_condition_symbol(data[date]['night']['condition'], 'night')} " \
                    f"{translate_condition(data[date]['night']['condition'])}, " \
                    f"{data[date]['night']['temperature']}°C\n"
-        message += f"    Днём (6 - 21 часов): \n" \
+        message += f"  • Днём (6 - 23 часов): \n" \
                    f"        {get_weather_condition_symbol(data[date]['day']['condition'], 'day')} " \
                    f"{translate_condition(data[date]['day']['condition'])}, " \
-                   f"{data[date]['day']['temperature']}°C\n"
+                   f"{data[date]['day']['temperature']}°C\n\n"
     return message
 
 
